eac64d48d43dfb5ad9e1c4ead434944670cc93d8
aman baviskar <amanbaviskar1@gmail.com>
2023-02-02 17:13:43 +0530

    again again 2 commit

--- prime.java
+++ prime.java
@@ -3,6 +3,7 @@ public class prime {
         int i, m = 0, flag = 0;
         int n = 3;
         m = n / 2;
+        // jajajaj
         if (n == 0 || n == 1) {
             System.out.println(n + "is not a prime number");
         } else {
